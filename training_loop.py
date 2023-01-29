eefafa20942446de96aa03bd54408e30722a276f
Justin Xu <52216145+justin13601@users.noreply.github.com>
2023-01-29 03:02:40 -0500

    added training loop
    accuracy calculation WIP, using F1 and confusion matrix (classification_report), but need to find a way to return a value and save for plotting (which will have 8 datapoints per epoch for both train/val)

--- training_loop.py
+++ training_loop.py
@@ -68,3 +68,107 @@ def plot_training_curve(path):
     plt.ylabel("Loss")
     plt.legend(loc='best')
     plt.show()
+
+    
+    def get_accuracy(net, loader, label_names):
+    for i, data in enumerate(loader, 0):
+        inputs, labels = data
+        if use_cuda and torch.cuda.is_available():
+          inputs= inputs.cuda()
+          labels = labels.cuda()
+          net = net.cuda()
+        outputs = net(inputs)
+        labels.float()
+        classification_report(
+            labels,
+            outputs,
+            output_dict=False,
+            target_names=label_names
+        )
+    return
+
+  ###############################################################################
+def evaluate(net, loader, criterion):
+    """ Evaluate the network on the validation set.
+
+     Args:
+         net: PyTorch neural network object
+         loader: PyTorch data loader for the validation set
+         criterion: The loss function
+     Returns:
+         err: A scalar for the avg classification error over the validation set
+         loss: A scalar for the average loss function over the validation set
+     """
+    for i, data in enumerate(loader, 0):
+        inputs, labels = data
+        if use_cuda and torch.cuda.is_available():
+          inputs= inputs.cuda()
+          labels = labels.cuda()
+          net = net.cuda()
+        outputs = net(inputs)
+        loss_func = nn.BCELoss()
+        loss = loss_func(outputs, labels.float())
+        loss.backward()
+    return_loss = float(loss)
+    return return_loss
+   
+def train_net(net, train_loader, val_loader, batch_size=150, learning_rate=0.005, num_epochs=6):
+    # Define the Loss function and optimizer
+    # Optimizer will be SGD with Momentum.
+    criterion = nn.BCELoss()
+    optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9)
+    ########################################################################
+    # Set up some numpy arrays to store the training/test loss/erruracy
+    train_acc = np.zeros(num_epochs)
+    train_loss = np.zeros(num_epochs)
+    val_acc = np.zeros(num_epochs)
+    val_loss = np.zeros(num_epochs)
+    ########################################################################
+    # Train the network
+    # Loop over the data iterator and sample a new batch of training data
+    # Get the output from the network, and optimize our loss function.
+    start_time = time.time()
+    for epoch in range(num_epochs):  # loop over the dataset multiple times
+        total_train_loss = 0.0
+        total_train_err = 0.0
+        total_epoch = 0
+        i=0
+        for text, labels in iter(train_loader):
+            if use_cuda and torch.cuda.is_available():
+              text = text.cuda()
+              labels = labels.cuda()
+              net = net.cuda()
+            # Zero the parameter gradients
+            optimizer.zero_grad()
+            # Forward pass, backward pass, and optimize
+            outputs = net(text)
+            loss = criterion(outputs, labels.float())
+            loss.backward()
+            optimizer.step()
+            i=i+1
+        train_loss[epoch] = float(loss)
+        # train_acc[epoch] = get_accuracy(net, train_loader, label_names)
+        train_acc[epoch] = 0
+        # val_acc[epoch] = get_accuracy(net, valid_loader, label_names)
+        val_acc[epoch] = 0
+        val_loss[epoch] = evaluate(net, val_loader, criterion)
+        print(("Epoch {}: Train acc: {}, Train loss: {} | "+
+               "Validation acc: {}, Validation loss: {}").format(
+                   epoch + 1,
+                   train_acc[epoch],
+                   train_loss[epoch],
+                   val_acc[epoch],
+                   val_loss[epoch]))
+        # Save the current model (checkpoint) to a file
+        model_path = get_model_name(net.name, batch_size, learning_rate, epoch+1)
+        torch.save(net.state_dict(), model_path)
+    print('Finished Training')
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print("Total time elapsed: {:.2f} seconds".format(elapsed_time))
+    # Write the train/test loss/err into CSV file for plotting later
+    epochs = np.arange(1, num_epochs + 1)
+    np.savetxt("{}_train_err.csv".format(model_path), train_acc)
+    np.savetxt("{}_train_loss.csv".format(model_path), train_loss)
+    np.savetxt("{}_val_err.csv".format(model_path), val_acc)
+    np.savetxt("{}_val_loss.csv".format(model_path), val_loss)
